5cb783d905776efaed604dd52f14ce1b12d4a14b
philippe MEYER <pmg.meyer@gmail.com>
2024-03-05 02:42:46 +0100

    Update main_script.js
    Working on Mutation observer. Found no way to 'debounce' it added isRunning, hiding bots ratings and chat ratings

--- main_script.js
+++ main_script.js
@@ -1,22 +1,23 @@
 
 // ***********************************************************
-// Inspired by https://github.com/triton11/ChessRating
+// Inspired by https://github.com/triton11/ChessTier
 // ***********************************************************
 
 let noRating = true;
+let isRunning = false;
 
-function launchObserver() {
-  var observer = new MutationObserver(function (mutations) {
+const launchObserver = () => {
+  const observer = new MutationObserver(function (mutations) {
     if (window.location.pathname.includes("/game/") || window.location.pathname.includes("/play/")) {
       // only in games not in lists because otherwise it would freeze chrome
-      run();
+      if (!isRunning) run();
     }
   });
   observer.observe(document, { childList: true, subtree: true });
 }
 
-function run() {
-
+const run = () => {
+  isRunning = true;
   // Hide rating in games
   const gameCollection = document.querySelectorAll('div.user-tagline-component');
 
@@ -36,6 +37,20 @@ function run() {
       ratingTag.style.display = 'none';
     }
   }
+  // Hide chat ratings (from ChessTier)
+  const chatStartRatings = document.querySelectorAll('div.live-game-start-component')
+  for (let i = 0; i < chatStartRatings.length; i++) {
+    if (chatStartRatings[i].style.display !== 'none') chatStartRatings[i].style.display = 'none';
+  }
+  const chatEndRatings = document.querySelectorAll('div.live-game-over-component')
+  for (let i = 0; i < chatEndRatings.length; i++) {
+    if (chatEndRatings[i].style.display !== 'none') chatEndRatings[i].style.display = 'none';
+  }
+
+  const botRating = document.querySelector('div.selected-bot-component span.selected-bot-rating');
+  if (botRating !== null && botRating.style.display !== 'none') botRating.style.display = 'none';
+
+  isRunning = false;
 }
 
 
